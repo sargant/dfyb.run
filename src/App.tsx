d18458858f68320cfdd3d77c948b21eea73c1198
Robert Sargant <robert@sargant.com>
2021-11-30 09:46:28 +0000

    Actual initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App: React.FC = () => (
+  <>
+    <h1>Hi</h1>
+    <a href="/api/getBarcode">
+      <img src="/add-to-apple-wallet.svg" height="50" />
+    </a>
+  </>
+);
+
+export default App;
