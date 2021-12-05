287f1a85c17366e91de76e40c9611aef156ab031
Robert Sargant <robert@sargant.com>
2021-12-05 20:12:36 +0000

    Fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,6 @@ module.exports = {
     sourceType: 'module',
     project: './tsconfig.json'
   },
-  ignorePatterns: [
-    '.eslintrc.js'
-  ],
   settings: {
     react: {
       version: 'detect'
