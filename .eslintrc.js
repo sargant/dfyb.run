bb0c57b3c34909ce359c71db3eb893a737d8136f
Robert Sargant <robert@sargant.com>
2021-12-05 18:54:25 +0000

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  extends: [
+    'eslint:recommended',
+    'standard',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ]
+}
