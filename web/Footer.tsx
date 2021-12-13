fceee34a65b70a734abff6b1aa50176b41f9ce7e
Rob Sargant <robert@sargant.com>
2021-12-13 09:06:51 +0000

    Fix icons

--- web/Footer.tsx
+++ web/Footer.tsx
@@ -1,16 +1,17 @@
-import Icon from '@mdi/react'
-import { mdiEmail, mdiGithub, mdiTwitter } from '@mdi/js'
+import IconGithub from '~icons/fa-brands/github-square'
+import IconTwitter from '~icons/fa-brands/twitter-square'
+import IconEnvelope from '~icons/fa-solid/envelope-square'
 
 const Footer: React.FC = () => (
-  <div className="flex flex-row justify-center my-16">
-    <a className="block mx-4 hover:text-secondary" href="mailto:hello@dfyb.run">
-      <Icon path={mdiEmail} size={2} />
+  <div className="flex flex-row justify-center my-16 children:(block mx-8) svg:(text-4xl inline-block mr-2)">
+    <a className="hover:text-secondary" href="https://github.com/sargant/dfyb.run">
+      <IconGithub /> /sargant/dfyb.run
     </a>
-    <a className="block mx-4 hover:text-secondary" href="https://twitter.com/dfyb_run">
-      <Icon path={mdiTwitter} size={2} />
+    <a className="hover:text-secondary" href="https://twitter.com/dfyb_run">
+      <IconTwitter /> @dfyb_run
     </a>
-    <a className="block mx-4 hover:text-secondary" href="https://github.com/sargant/dfyb.run">
-      <Icon path={mdiGithub} size={2} />
+    <a className="hover:text-secondary" href="mailto:hello@dfyb.run">
+      <IconEnvelope /> hello@dfyb.run
     </a>
   </div>
 )
