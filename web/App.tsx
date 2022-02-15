67931dc5f9935c7a8783a4408853228ac919054f
Rob Sargant <robert@sargant.com>
2022-02-15 09:42:24 +0000

    🙄

--- web/App.tsx
+++ web/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import * as Pages from './pages'
 import Header from './Header'
