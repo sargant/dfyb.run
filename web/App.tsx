a0ac5b7484357623425afa3dff1b5aa4e558c14c
Rob Sargant <robert@sargant.com>
2022-12-02 11:12:27 +0000

    Disable pass generation for now

--- web/App.tsx
+++ web/App.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import * as Pages from './pages'
-import Header from './Header'
-import Footer from './Footer'
+import { Header } from './Header'
+import { Footer } from './Footer'
 
-const App: React.FC = () => (
-  <>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Pages.Home />} />
-      <Route path="/faq" element={<Pages.FAQ />} />
-      <Route path="/privacy" element={<Pages.Privacy />} />
-      <Route path="*" element={<Navigate to="/" replace={true} />} />
-    </Routes>
-    <Footer />
-  </>
-)
-
-export default App
+export const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Pages.Home />} />
+        <Route path="/faq" element={<Pages.FAQ />} />
+        <Route path="/privacy" element={<Pages.Privacy />} />
+        <Route path="*" element={<Navigate to="/" replace={true} />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
