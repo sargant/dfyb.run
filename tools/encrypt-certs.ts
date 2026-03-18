8eb3d4853d49c990298cf629d420b3d188e03544
Robert Sargant <robert@sargant.com>
2026-03-18 20:59:08 +0000

    chore: update dependencies and devDependencies in package.json
    fix: change output file extension from .js to .ts in encrypt-certs.ts
    
    refactor: update tsconfig.json for module resolution and include paths
    
    style: update gray color to neutral in windi.config.ts
    
    feat: add TypeScript declaration file for WindiCSS in windicss-fix.d.ts

--- tools/encrypt-certs.ts
+++ tools/encrypt-certs.ts
@@ -21,8 +21,8 @@ const signerKey = readFileSync(join(__dirname, 'pass.run.dfyb.key'))
 const signerKeyEncrypted = encrypt(signerKey, key)
 output += `export const signerKey = '${signerKeyEncrypted.data}'\n`
 
-writeFileSync(join(__dirname, '..', 'api', 'certs.enc.js'), output)
+writeFileSync(join(__dirname, '..', 'api', 'certs.enc.ts'), output)
 
-console.log('Successfully encrypted to /pass-models/certs.enc.json')
+console.log('Successfully encrypted to /pass-models/certs.enc.ts')
 console.log('The following encryption key will only be shown once - make sure to copy it')
 console.log(`Key: ${key.toString('base64')}`)
